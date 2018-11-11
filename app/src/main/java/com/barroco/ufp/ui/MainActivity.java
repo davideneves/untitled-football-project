176ee8a33a7816353d514a26f776ddb1af085127
Davide Neves <d.neves28@gmail.com>
2018-11-11 17:03:45 +0000

    Project setup with base libraries

--- app/src/main/java/com/barroco/ufp/ui/MainActivity.java
+++ app/src/main/java/com/barroco/ufp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.barroco.ufp;
+package com.barroco.ufp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.barroco.ufp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/barroco/ufp/util/AppUtil.java
@@ -0,0 +1,4 @@
+package com.barroco.ufp.util;
+
+public class AppUtil {
+}
